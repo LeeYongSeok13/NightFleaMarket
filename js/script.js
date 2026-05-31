972ddb876d5af589c4d4dc4021d8abdb4dea7979
LeeYongSeok13 <minamiharuka333@gmail.com>
2026-05-31 22:55:29 +0900

    헤더, 슬라이드 작업

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+  // 헤더
+  $("nav>ul>li").mouseenter(function () {
+    $(this).children(".sub").stop().fadeIn();
+  });
+  $("nav>ul>li").mouseleave(function () {
+    $(".sub").stop().fadeOut();
+  });
+
+  // 슬라이드
+  let i = 0;
+  function slide() {
+    if (i < 2) {
+      i++;
+    } else {
+      i = 0;
+    }
+    $(".slide ul").animate({ marginTop: -350 * i }, 1000);
+  }
+
+  setInterval(slide, 3000);
+});
